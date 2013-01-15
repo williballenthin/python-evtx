738568b940f5f30e4d2e403f12ddb86628e43156
Willi Ballenthin <willi.ballenthin@gmail.com>
2013-01-15 03:49:58 +0000

    prep for dist

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name="Evtx",
+      version="0.1",
+      description="Pure Python parser for recent Windows event log files (.evtx).",
+      author="Willi Ballenthin",
+      author_email="willi.ballenthin@gmail.com",
+      url="https://github.com/williballenthin/python-evtx",
+      license="Apache 2.0 License")
