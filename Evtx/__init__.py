9fceb711a15850d386d8bbf8161f8b87983ff442
Willi Ballenthin <willi.ballenthin@gmail.com>
2013-05-31 02:35:47 +0000

    version bump, tag 0.3.0

--- Evtx/__init__.py
+++ Evtx/__init__.py
@@ -14,10 +14,11 @@
 #   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
-__version__ = '0.2.4'
+__version__ = '0.3.0'
 
 __all__ = [
     'Evtx',
     'BinaryParser',
     'Nodes',
+    'Views',
 ]
