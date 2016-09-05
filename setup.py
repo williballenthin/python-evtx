d7a43e828478e04f561f187900bb06b7d4458141
Willi Ballenthin <wilbal1087@gmail.com>
2016-09-05 18:54:11 -0400

    setup: use find_packages

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 
-from setuptools import setup
+import setuptools
 
 long_description="""python-evtx is a pure Python parser for recent Windows Event Log files (those with the file extension ".evtx"). The module provides programmatic access to the File and Chunk headers, record templates, and event entries. For example, you can use python-evtx to review the event logs of Windows 7 systems from a Mac or Linux workstation. The structure definitions and parsing strategies were heavily inspired by the work of Andreas Schuster and his Perl implementation "Parse-Evtx"."""
 
-setup(name="python-evtx",
+setuptools.setup(name="python-evtx",
       version="0.3.2",
       description="Pure Python parser for recent Windows event log files (.evtx).",
       long_description=long_description,
@@ -12,4 +12,4 @@ setup(name="python-evtx",
       author_email="willi.ballenthin@gmail.com",
       url="https://github.com/williballenthin/python-evtx",
       license="Apache 2.0 License",
-      packages=["Evtx"])
+      packages=setuptools.find_packages())
