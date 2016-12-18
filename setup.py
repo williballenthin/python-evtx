edb2b7d45eec9ff849217f6e0f8fb24fcfe81bdf
Willi Ballenthin <willi.ballenthin@gmail.com>
2016-12-17 21:08:00 -0500

    bump to version 0.5.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import setuptools
 long_description="""python-evtx is a pure Python parser for recent Windows Event Log files (those with the file extension ".evtx"). The module provides programmatic access to the File and Chunk headers, record templates, and event entries. For example, you can use python-evtx to review the event logs of Windows 7 systems from a Mac or Linux workstation. The structure definitions and parsing strategies were heavily inspired by the work of Andreas Schuster and his Perl implementation "Parse-Evtx"."""
 
 setuptools.setup(name="python-evtx",
-      version="0.5.1",
+      version="0.5.2",
       description="Pure Python parser for recent Windows event log files (.evtx).",
       long_description=long_description,
       author="Willi Ballenthin",
