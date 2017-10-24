bf2b9b54b4ee2fcfb0e4c30e04149a51ec423c84
Willi Ballenthin <willi.ballenthin@gmail.com>
2017-10-24 12:22:14 -0400

    add regression test demonstrating issue 43
    ref: #43

---
+++ tests/test_issue_43.py
@@ -0,0 +1,26 @@
+import os
+import pytest
+
+import Evtx.Evtx as evtx
+
+from fixtures import *
+
+
+def get_record_by_num(log, record_num):
+    for record in log.records():
+        if record.record_num() == record_num:
+            return record
+    raise KeyError(record_num)
+
+
+def test_issue_43(data_path):
+    '''
+    regression test demonstrating issue 43.
+
+    Args:
+      data_path (str): the file system path of the test directory.
+    '''
+    with evtx.Evtx(os.path.join(data_path, 'issue_43.evtx')) as log:
+        bad_rec = get_record_by_num(log, 508)
+        _ = bad_rec.xml()
+
