953520633f99c450253e8d7142d18e7fce03b684
Willi Ballenthin <willi.ballenthin@gmail.com>
2024-06-24 08:05:30 +0000

    v0.8.0

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ and his Perl implementation "Parse-Evtx"."""
 
 setuptools.setup(
         name="python-evtx",
-        version="0.7.4",
+        version="0.8.0",
         description="Pure Python parser for recent Windows event log files (.evtx).",
         long_description=long_description,
         author="Willi Ballenthin",
